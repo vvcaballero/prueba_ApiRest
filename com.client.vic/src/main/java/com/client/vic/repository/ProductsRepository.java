f1664c145f1f8db859202ce8b5560179ec817413
Victor Manuel <vvcaballero07@gmail.com>
2022-02-21 15:00:15 -0600

    Api prueba

--- com.client.vic/src/main/java/com/client/vic/repository/ProductsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.client.vic.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.client.vic.entity.Product;
-
-public interface ProductsRepository extends JpaRepository<Product, Long>{
-
-}
